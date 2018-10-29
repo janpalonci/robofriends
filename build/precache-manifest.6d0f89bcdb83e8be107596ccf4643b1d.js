80eb8c7773ed57996c0854fd2d8cdf6503767e08
Jan Palonci <jan.palonci@github.com>
2018-10-29 16:31:55 -0700

    final lesson

---
+++ build/precache-manifest.6d0f89bcdb83e8be107596ccf4643b1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "022e718b49ea27042218",
+    "url": "/static/js/main.022e718b.chunk.js"
+  },
+  {
+    "revision": "50caed98750b25b168fd",
+    "url": "/static/js/1.50caed98.chunk.js"
+  },
+  {
+    "revision": "022e718b49ea27042218",
+    "url": "/static/css/main.9b12cb2e.chunk.css"
+  },
+  {
+    "revision": "50caed98750b25b168fd",
+    "url": "/static/css/1.f9948346.chunk.css"
+  },
+  {
+    "revision": "7d5c9354611a1e1997604d5d8a6ef81d",
+    "url": "/index.html"
+  }
+];
